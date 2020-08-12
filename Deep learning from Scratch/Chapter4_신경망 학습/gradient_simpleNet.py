f12d8993f6c82f579b486f845da3e0ecf3ea75ca
yskim0 <wx12348@gmail.com>
2020-08-12 19:11:37 +0900

    DL chapter4 update

---
+++ Deep learning from Scratch/Chapter4_신경망 학습/gradient_simpleNet.py
@@ -0,0 +1,19 @@
+import sys, os
+sys.path.append(os.pardir)
+import numpy as np 
+from common.functions import softmax, cross_entropy_error
+from common.gradient import numberical_gradient
+
+class simpleNet:
+    def __init__(self):
+        self.W = np.random.randn(2,3) # 정규 분포로 초기화
+    
+    def predict(self, x):
+        return np.dot(x, self.W)
+    
+    def loss(self, x, t):
+        z = self.predict(x)
+        y = softmax(z)
+        loss = cross_entropy_error(y, t)
+
+        return loss
